d52b4afee6ecbf78c69d055937c8e5cd80b8c028
Sanghee Kim <sangheestyle@gmail.com>
2015-02-28 19:03:30 -0700

    table can also take a list of objects #9
    table can also take a list of objects, where each row is an object and
    each property of the object is a column #9

--- examples.js
+++ examples.js
@@ -21,6 +21,9 @@ domout.svg(viz.linegraph(data))
 domout.html('<h1>Line Graph with 2d array</h1>')
 domout.svg(viz.linegraph(data2))
 
+domout.html('<h1>Line Graph with 2d array and custom color</h1>')
+domout.svg(viz.linegraph(data2, {color:'rgba(0,0,255,0.3)'}))
+
 domout.html('<h1>List</h1>')
 domout.html(viz.list(data))
 
@@ -30,13 +33,18 @@ var data = [
     ['Adam', 4, 8]
 ]
 
-
 domout.html(viz.json(data))
 
 domout.html('<h1>Table</h1>')
 var header = ['name', 'item1', 'item2']
-domout.html(viz.table(header, data, true))
+domout.html(viz.table(data, {header: header, grid:true}))
 
+domout.html('<h1>Table with objects</h1>')
+var students = [
+  {name: 'John', age: 22, city: 'Boulder'},
+  {name: 'Adam', age: 23, city: 'Longmont'}
+]
+domout.html(viz.table(students))
 
 var data = [
     [5, 5],
